29ddb02a648f10d157d0fb097b11dfebe773fc2b
Venkata Krishna Vanamamalai <110457541+Venky2804@users.noreply.github.com>
2024-02-01 20:33:39 +0530

    Update hisat2.py

--- util/hisat2.py
+++ util/hisat2.py
@@ -6,7 +6,7 @@ def run(seq, hisat2, t, hs2db, x, hsres):
     if seq == "S":
             hs = hisat2+" -q -p "+t+" --dta -x "+hs2db+" -U ../qc/"+x+".fastq -S ../hs2/"+x+".sam --summary-file ../hs2/"+x+"_align_summary"
     elif seq == "P":
-        hs = hisat2+" -q -p "+t+" --dta -x "+hs2db+" -1 ../qc/"+x+"_1.fastq -2 ../qc/"+x+"_2.fastq -S ../hs2/"+x+".sam --summary-file ../hs2/"+x+"_align_summary"
+        hs = hisat2+" -q -p "+t+" --dta -x "+hs2db+" -1 ../qc/"+x+"_R1.fastq -2 ../qc/"+x+"_R2.fastq -S ../hs2/"+x+".sam --summary-file ../hs2/"+x+"_align_summary"
     hs1 = hs.split()
     s2 = subprocess.run(hs1, stderr=subprocess.DEVNULL)
     if not s2.returncode == 0:
